931f749bb16eca8ffb12d1a5216143f988c70221
Bogi Prasetyandi <bogi.prasetyandi@rocketmail.com>
2024-04-16 21:28:05 +0700

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/signin' element={<Signin />}/>
+        <Route path='/signup' element={<SignUp />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/profile' element={<Profile />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
   
